af0dc5934171fd0df732e8324eab574ec02d546d
jakub.kaspar <jakub.kaspar@ubk.cz>
2021-03-19 14:55:34 +0100

    Help generate by command arguments (now only required, soon also optional)

--- src/main/java/me/inao/discordbot/command/Help.java
+++ src/main/java/me/inao/discordbot/command/Help.java
@@ -9,28 +9,34 @@ import org.javacord.api.entity.message.embed.EmbedBuilder;
 
 import java.util.List;
 
-public class Help implements ICommand{
+public class Help implements ICommand {
     @Override
     public void onCommand(Main instance, Message message, List<IParameter> args) {
         EmbedBuilder builder = new EmbedBuilder();
         builder.setDescription("Helping everyone is my pleasure, you know :eyes:").setTitle("Help");
         builder.setFooter("Prefix for commands is " + instance.getConfig().getPrefix());
 
-        instance.getLoader().getLoadedCommands().entrySet().iterator().forEachRemaining(st->{
-            if(instance.getConfig().isCommandEnabled(st.getKey())){
-                if(st.getValue().getUsage() != null){
-                    builder.addField(st.getKey(), st.getKey() + " " + st.getValue().getUsage());
-                }else{
-                    builder.addField(st.getKey(), "No parameters");
+        instance.getLoader().getLoadedCommands().entrySet().iterator().forEachRemaining(st -> {
+            if (instance.getConfig().isCommandEnabled(st.getKey())) {
+                if (!st.getKey().equals(this.getClass().getSimpleName())) {
+
+                    StringBuilder usage = new StringBuilder();
+                    if (st.getValue().requiredParameters().length > 0) {
+                        for (Class<? extends IParameter> param : st.getValue().requiredParameters()) {
+                            try {
+                                usage.append(" < -").append(String.join("|-", param.getDeclaredConstructor().newInstance().getIdentifiers())).append(" >");
+                            } catch (Exception e) {
+                                e.printStackTrace();
+                            }
+                        }
+                        builder.addField(st.getKey(), usage.toString());
+                    } else {
+                        builder.addField(st.getKey(), "No parameters");
+                    }
                 }
             }
         });
         new MessageBuilder().setEmbed(builder).send(message.getChannel());
-    }
-
-    @Override
-    public String getUsage() {
-        return null;
     }
 
     @Override
