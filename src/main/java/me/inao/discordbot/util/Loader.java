e8af296dfa82edd5382fe5cb8a3c8a523982db6f
Jakub <onemoreplayscz@gmail.com>
2021-05-24 20:33:50 +0200

    WIP: Slash commands

--- src/main/java/me/inao/discordbot/util/Loader.java
+++ src/main/java/me/inao/discordbot/util/Loader.java
@@ -21,6 +21,7 @@ public class Loader {
     private Map<String, ICommand> loadedCommands;
     private final Main main;
     private final DiscordApiBuilder builder;
+
     public void loadListeners(String prefix) throws NoSuchMethodException, IllegalAccessException, InvocationTargetException, InstantiationException {
         Reflections refl = new Reflections(prefix);
         Set<Class<? extends IListener>> classes = refl.getSubTypesOf(IListener.class);
@@ -29,36 +30,45 @@ public class Loader {
             new Logger(main, true, false, "Loaded event", "Loaded event: " + listener.getSimpleName(), Level.DEBUG);
         }
     }
+
     public void loadCommands(String prefix) throws NoSuchMethodException, IllegalAccessException, InvocationTargetException, InstantiationException {
         Reflections refl = new Reflections(prefix);
         Set<Class<? extends ICommand>> classes = refl.getSubTypesOf(ICommand.class);
         loadedCommands = new HashMap<>(classes.size());
         for (Class<? extends ICommand> command : classes) {
-            if(main.getConfig().isCommandEnabled(command.getSimpleName())){
+            if (main.getConfig().isCommandEnabled(command.getSimpleName())) {
                 loadedCommands.put(command.getSimpleName(), command.getDeclaredConstructor().newInstance());
                 new Logger(main, true, false, "Loaded command", "Loaded command: " + command.getSimpleName(), Level.DEBUG);
             }
         }
     }
-    public HashMap<List<String>, IParameter> loadParams(String prefix){
+
+    public HashMap<List<String>, IParameter> loadParams(String prefix) {
         HashMap<List<String>, IParameter> map = new HashMap<>();
         Reflections reflections = new Reflections(prefix);
         Set<Class<? extends IParameter>> classes = reflections.getSubTypesOf(IParameter.class);
 
-        for (Class<? extends IParameter> param : classes){
-           try{
-               IParameter instance = param.getDeclaredConstructor().newInstance();
-               map.put(Arrays.asList(instance.getIdentifiers()), instance);
-           }catch (Exception e){
-               e.printStackTrace();
-           }
+        for (Class<? extends IParameter> param : classes) {
+            try {
+                IParameter instance = param.getDeclaredConstructor().newInstance();
+                map.put(Arrays.asList(instance.getIdentifiers()), instance);
+            } catch (Exception e) {
+                e.printStackTrace();
+            }
         }
         return map;
     }
-    public void loadSlashCommands(){
-        loadedCommands.forEach((n, c) -> new ApplicationCommandBuilder().
-                setName(n).
-                setDescription(main.getConfig().getCommandDescription(n)).
-                createGlobal(main.getApi()));
+
+    public void loadSlashCommands() {
+        loadedCommands.forEach((n, c) -> {
+            ApplicationCommandBuilder builder = new ApplicationCommandBuilder().setName(n.toLowerCase(Locale.ROOT));
+            if (main.getConfig().getCommandDescription(n) != null) {
+                builder.setDescription(main.getConfig().getCommandDescription(n));
+                builder.createGlobal(main.getApi()).exceptionally(fn -> {
+                    fn.printStackTrace();
+                    return null;
+                });
+            }
+        });
     }
 }
